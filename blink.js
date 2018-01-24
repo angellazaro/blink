e0745dfcb9f45f3d06f6cc4a66a4d7ecbf7e1cb2
Ángel C. Lázaro <angelcristoballazarorios@gmail.com>
2018-01-24 15:28:45 +0100

    Se suben las correcciones con la imagen de Docker NodeJS

---
+++ blink.js
@@ -0,0 +1,11 @@
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
+var blinkInterval = setInterval(blinkLED, 1000);
+
+function blinkLED(){
+	if(LED.readSync() === 0){
+		LED.writeSync(1);
+	}else{
+		LED.writeSync(0);
+	}
+}
